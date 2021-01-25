c6873c659b54969d34343cf4bab7a1ecd51860c2
A Emmanual Sarker <emma@As-MacBook-Pro.local>
2021-01-26 02:00:45 +0600

    updated

--- javascript.js
+++ javascript.js
@@ -11,20 +11,29 @@ loginBtn.addEventListener("click",function(){
 
 const depositBtn=document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
-    const depositAmount=document.getElementById("depositAmount").value;
-    const depositNumber=parseFloat(depositAmount);
-
-    const currentDeposit=document.getElementById("currentDeposit").innerText;
-    const currentDepositNumber=parseFloat(currentDeposit);
-
-    const totalDeposit=depositNumber+currentDepositNumber;
-    document.getElementById("currentDeposit").innerText=totalDeposit;
-
-
-    const currentBalance=document.getElementById("currentBalance").innerText;
-    const currentBalanceNumber=parseFloat(currentBalance);
-    const totalBalance=depositNumber+currentBalanceNumber;
-    document.getElementById("currentBalance").innerText=totalBalance;
+    const depositNumber=getInputNumber("depositAmount");
+    updateSpanText("currentDeposit",depositNumber);
+    updateSpanText("currentBalance",depositNumber);
     document.getElementById("depositAmount").value="";
 
 })
+
+//withdraw button event handler
+
+const withdrawBtn=document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click",function(){
+    const widthdrawNumber=getInputNumber("withdrawAmount");
+})
+
+function getInputNumber(id){
+    const amount=document.getElementById(id).value;
+    const amountNumber=parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber){
+    const current=document.getElementById(id).innerText;
+    const currentNumber=parseFloat(current);
+    const totalAmount=depositNumber+currentNumber;
+    document.getElementById(id).innerText=totalAmount;
+}
